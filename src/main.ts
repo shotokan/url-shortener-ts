76b3748289a467531c8564f55037d5506dc57e68
ivan sabido <shotokan.isc@gmail.com>
2020-07-03 20:12:30 -0500

    test husky

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
-console.log('hello');
+import express = require('express');
+// Create a new express app instance
+const app: express.Application = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000);
